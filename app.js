49c20a6ba1b28e4ba7c0db3f09c2ab6401980703
eze <91556738+dEzequiel@users.noreply.github.com>
2022-12-30 21:26:51 +0100

    Dynamically load routes in Express

---
+++ app.js
@@ -0,0 +1,17 @@
+const databaseModule = require("./config/mongoConfig.js");
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 3000;
+const app = express();
+const routes = require("./routes");
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+
+databaseModule.connect();
