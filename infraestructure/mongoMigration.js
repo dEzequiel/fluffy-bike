9afb0920f59c657f46fbb0dfc7012ddf31c8dfb1
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-01 20:16:07 +0100

    Prepare migration script for development database

---
+++ infraestructure/mongoMigration.js
@@ -0,0 +1,35 @@
+const { MongoClient } = require("mongodb");
+const mockedBycicles = require("../tests/json_collection/bycicles");
+const dotenv = require("dotenv");
+dotenv.config();
+
+let client;
+let db;
+
+async function migrate() {
+  client = new MongoClient(process.env.MONGO_URI_DEVELOPMENT);
+  try {
+    await client.connect();
+    db = client.db("BikehubDB");
+    const collection = db.collection("Testing");
+
+    let documents = await collection.estimatedDocumentCount();
+    if (documents > 0)
+      await collection.drop().then((status) => {
+        console.log(`Drop testing collection => ${status}`);
+      });
+
+    // Actual inserting documents.
+    await collection.insertMany(mockedBycicles);
+    if (documents > 0) {
+      console.log("Seeding testing collection complete!");
+    }
+    client.close();
+  } catch (ex) {
+    console.error(ex.message);
+  }
+}
+
+migrate().catch((err) => {
+  console.log(err);
+});
