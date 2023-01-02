c2ff76efbfb0e7eb59e950c819aefe85d66dc2f1
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-02 16:23:24 +0100

    Refactor Model data access test

--- tests/dataaccess.test.js
+++ tests/dataaccess.test.js
@@ -1,57 +1,33 @@
-const mongoose = require("mongoose");
 const databaseModule = require("../infraestructure/mongoConfig.js");
 const { expect } = require("@jest/globals");
 const Model = require("../models/nosql/bycicle.js");
 
+let connection;
 describe("Mongoose testing", () => {
-  let connection;
-  let db;
-  const model = require("../models/nosql/bycicle.js");
-  model.collection = "Testing";
-
   beforeAll(async () => {
-    connection = await databaseModule.connect();
-    db = databaseModule.getDb();
+    await databaseModule.connect();
   });
 
   afterAll(async () => {
-    await db.close();
+    await databaseModule.disconnect();
   });
 
-  test("Should stay connecting to database", async () => {
-    expect(db.readyState).toEqual(2);
+  connection = databaseModule.getConnection();
+
+  test("Should connect to database", async () => {
+    expect(connection.readyState).toEqual(1); // Status connected. Not connecting, is different.
   });
 
-  test("Model should use testing collection", () => {
-    expect(model.collection).toEqual("Testing");
+  test("Model should use testing collection", async () => {
+    expect(Model.collection.collectionName).toEqual("Testing");
   });
 
-  test("Model should use db connection object", () => {
-    expect(model.db).toBe(db);
+  test("Should know how many documents exists in testing collection", async () => {
+    const count = await Model.estimatedDocumentCount();
+    expect(count).toEqual(6);
   });
+});
 
-  // test("Should know how many documents exists in collection",  () => {
-  //   expect(model.estimatedDocumentCount().count()).toEqual(6);
-  // });
-  test("Should insert new document", () => {
-    const doc = new Model({
-      name: "Roadster Elite",
-      brand: "Argon 18",
-      price: 1299.99,
-      type: "Road",
-      frame: "Carbon",
-      fork: "XC",
-      gears: "Derailleur gears",
-      brakes: "Hydraulic Disc",
-      wheels: "700c",
-      tires: "Road tires",
-      suspension: "Hardtail",
-      weight: 7.5,
-      available: true,
-    });
-
-    model.create(doc);
-
-    expect(doc._id).toBeDefined();
-  });
+test("Should disconnect from database", () => {
+  expect(connection.readyState).toEqual(0); // Outside describe() because it's neede to run afterAll().
 });
