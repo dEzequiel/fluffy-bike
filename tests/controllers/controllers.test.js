dbfb631dc0788ea86bae300adae99d4d6c689592
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-02 20:25:09 +0100

    Skeleton controller test

--- tests/controllers/controllers.test.js
+++ tests/controllers/controllers.test.js
@@ -1,11 +1,20 @@
 const { expect } = require("@jest/globals");
 const request = require("supertest");
 const app = require("./server.js");
+const databaseModule = require("../../infraestructure/mongoConfig.js");
 
 // Usage of jest sintaxis
 // Assertions occurs inside promise callback
 describe("Controllers testing", () => {
-  test("GET /skeleton/getAll", async () => {
+  beforeEach(async () => {
+    await databaseModule.connect();
+  });
+
+  afterEach(async () => {
+    await databaseModule.disconnect();
+  });
+
+  test("GET /skeleton should be Hello World message", async () => {
     // Arrange
     const expected = { message: "Hello World" };
 
@@ -15,6 +24,36 @@ describe("Controllers testing", () => {
       .expect(200)
       .then((res) => {
         // assertions
+        expect(res.body).toEqual(expected);
+      });
+  });
+
+  test("GET /skeleton/:id should be mongo obj", async () => {
+    // Arrange
+    const expected = {
+      _id: "63b1db13b2ade9465c9c5d0d",
+      name: "Roadster Elite",
+      brand: "Argon 18",
+      price: 1299.99,
+      type: "Road",
+      frame: "Carbon",
+      fork: "XC",
+      gears: "Derailleur gears",
+      brakes: "Hydraulic Disc",
+      wheels: "700c",
+      tires: "Road tires",
+      suspension: "Hardtail",
+      weight: 7.5,
+      available: true,
+    };
+
+    // Act and assert
+    await request(app)
+      .get(`/skeleton/${expected._id}`)
+      .expect(200)
+      .then((res) => {
+        // assertions
+        console.log(res.body);
         expect(res.body).toEqual(expected);
       });
   });
