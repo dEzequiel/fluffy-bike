eccbce53579f30f4cb2bb1c8c496a5295a7ba5e4
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-03 12:45:08 +0100

    Feature to retrieve one bycicle by id, handle errors

--- services/byciclesService.js
+++ services/byciclesService.js
@@ -9,28 +9,39 @@ const byciclesService = (function () {
     const { bycicle, id } = contextObject;
 
     // Get entity from the database
-    const result = await Model.findById(id).exec();
+    const entity = Model.findById(id).exec();
 
-    // Desacoplate response from database object structure (DAO)
-    const entity = destructDAO(result, {
-      includeId: true,
-      includeName: true,
-      includeBrand: true,
-      includePrice: true,
-      includeType: true,
-      includeFrame: true,
-      includeFork: true,
-      includeGears: true,
-      includeBrakes: true,
-      includeWheels: true,
-      includeTires: true,
-      includeSuspension: true,
-      includeWeight: true,
-      includeAvailable: true,
-    });
+    // Desacoplate Promise response from database object structure (DAO)
+    const result = entity
+      .then((doc) => {
+        if (doc) {
+          return destructDAO(doc, {
+            includeId: true,
+            includeName: true,
+            includeBrand: true,
+            includePrice: true,
+            includeType: true,
+            includeFrame: true,
+            includeFork: true,
+            includeGears: true,
+            includeBrakes: true,
+            includeWheels: true,
+            includeTires: true,
+            includeSuspension: true,
+            includeWeight: true,
+            includeAvailable: true,
+          });
+        } else {
+          return `No document found with id: ${contextObject.id}`;
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        return Promise.reject(err);
+      });
 
     // Return the entity
-    return entity;
+    return result;
   };
 
   // Public API for request handlers
