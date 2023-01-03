5002dd11ba9cb2503a90e13d496d3d80370786ed
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-03 12:43:23 +0100

    Add services test when _id is found and when is not found

--- tests/services/bycicles.service.test.js
+++ tests/services/bycicles.service.test.js
@@ -1,11 +1,14 @@
 const { expect } = require("@jest/globals");
 const infraestructure = require("../../infraestructure");
 const Model = require("../../models/nosql/bycicle.js");
+const ObjectId = require("mongodb").ObjectId;
+const services = require("../../services/");
 
 let connection;
 describe("Services testing", () => {
   beforeAll(async () => {
     await infraestructure.mongoMigrationConfig.connect();
+    await infraestructure.mongoMigrationConfig.dropCollection();
   });
 
   afterAll(async () => {
@@ -21,6 +24,73 @@ describe("Services testing", () => {
 
     test("Model should use testing collection", async () => {
       expect(Model.collection.collectionName).toEqual("Testing");
+    });
+  });
+
+  // Testing unit
+  describe("Services public API testing", () => {
+    test("Should return object with properties base on founded DAO", async () => {
+      // Arrange
+      const dataUnderTest = new Model({
+        _id: ObjectId("63b1db13b2ade9465c9c5d0d"),
+        name: "Roadster Elite",
+        brand: "Argon 18",
+        price: 1299.99,
+        type: "Road",
+        frame: "Carbon",
+        fork: "XC",
+        gears: "Derailleur gears",
+        brakes: "Hydraulic Disc",
+        wheels: "700c",
+        tires: "Road tires",
+        suspension: "Hardtail",
+        weight: 7.5,
+        available: true,
+      });
+
+      await dataUnderTest.save();
+
+      const contextObjectUnderTest = {
+        id: "63b1db13b2ade9465c9c5d0d",
+        name: "Roadster Elite",
+        brand: "Argon 18",
+        price: 1299.99,
+        type: "Road",
+        frame: "Carbon",
+        fork: "XC",
+        gears: "Derailleur gears",
+        brakes: "Hydraulic Disc",
+        wheels: "700c",
+        tires: "Road tires",
+        suspension: "Hardtail",
+        weight: 7.5,
+        available: true,
+      };
+
+      // Act
+      const result =
+        await services.byciclesService.bycicleServiceApi.getByIdAsync(
+          contextObjectUnderTest
+        );
+
+      // Assert
+      expect(Object.keys(result)).toEqual(Object.keys(contextObjectUnderTest));
+    });
+
+    test("Should return message becauses DAO was not found", async () => {
+      
+      // Arrange
+      const contextObjectUnderTest = { id: "13b1db13b2ade9465c9c5d0d" };
+      const expectedMessage = `No document found with id: ${contextObjectUnderTest.id}`;
+
+      //Act
+      const result =
+        await services.byciclesService.bycicleServiceApi.getByIdAsync(
+          contextObjectUnderTest
+        );
+
+      //Assert
+      expect(result).toEqual(expectedMessage);
     });
   });
 });
