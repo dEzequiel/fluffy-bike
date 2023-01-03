67c3ce712230bc49e0abb885d1182a2b1d71e08b
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-03 20:30:14 +0100

    Use of mongo data acces layer api

--- services/byciclesService.js
+++ services/byciclesService.js
@@ -1,5 +1,7 @@
 const Model = require("../models/nosql/bycicle.js");
 const destructDAO = require("../utils/destructBycicleDAO.js");
+const mongoDataAccessLayer =
+  require("../infraestructure/data-access/mongo.bycicles.layer.js").MongoDataAccesApi;
 
 ////// Module pattern
 const byciclesService = (function () {
@@ -9,36 +11,10 @@ const byciclesService = (function () {
     const { bycicle, id } = contextObject;
 
     // Get entity from the database
-    const entity = Model.findById(id).exec();
+    const entity = await mongoDataAccessLayer.findByIdAsync(id);
 
-    // Desacoplate Promise response from database object structure (DAO)
-    const result = entity
-      .then((doc) => {
-        if (doc) {
-          return destructDAO(doc, {
-            includeId: true,
-            includeName: true,
-            includeBrand: true,
-            includePrice: true,
-            includeType: true,
-            includeFrame: true,
-            includeFork: true,
-            includeGears: true,
-            includeBrakes: true,
-            includeWheels: true,
-            includeTires: true,
-            includeSuspension: true,
-            includeWeight: true,
-            includeAvailable: true,
-          });
-        } else {
-          return `No document found with id: ${contextObject.id}`;
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        return Promise.reject(err);
-      });
+    // Destructuring to receive only wanted properties
+    const result = destructDAO(entity);
 
     // Return the entity
     return result;
