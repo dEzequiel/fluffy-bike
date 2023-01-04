8374d386bbf30cb7cdcc3564f01192f5af839ad3
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-04 12:57:16 +0100

    Function constructor for api response

--- Application/utils/future-packages/ApiResponse.js
+++ Application/utils/future-packages/ApiResponse.js
@@ -1,7 +1,19 @@
-function ApiResponse() {
-  this.success = false;
-  this.statusCode = 0;
-  this.data = undefined;
+function ApiResponse(success, statusCode, data) {
+  this.success = success;
+  this.statusCode = statusCode;
+  this.data = data;
 }
+
+ApiResponse.prototype.setStatusCode = function (code) {
+  this.statusCode = code;
+};
+
+ApiResponse.prototype.setSucess = function (status) {
+  this.success = status;
+};
+
+ApiResponse.prototype.setData = function (data) {
+  this.statusCode = data;
+};
 
 module.exports = ApiResponse;
