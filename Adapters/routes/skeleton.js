61cb511c37a4177deeb0ec10916e73ded222256b
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-04 11:54:40 +0100

    Organize routes sequentallly. Add getAll express route

--- Adapters/routes/skeleton.js
+++ Adapters/routes/skeleton.js
@@ -3,8 +3,20 @@ const controllers = require("../controllers");
 
 const router = express.Router();
 
+// Actually if you pass anything after the /user/ path, eg, /users/anythingHere
+// would be matched by the /users/:id route. This is the way Express.js works. 
+// The routes you define are matched sequentially in the 
+// order they are defined. Hence,your second routing setup is correct.
+
 router.get("/", (req, res) => {
   res.send({ message: "Hello World" });
+});
+
+router.get("/getAll", async (req, res) => {
+  const resource =
+    await controllers.byciclesController.bycicleControllerApi.getAll();
+
+  return res.status(200).json(resource);
 });
 
 router.get("/:id", async (req, res) => {
