9136bff5dfcc6ebd9de09f20094c7622b93d5f78
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-05 18:10:42 +0100

    Create repository implementation

---
+++ Domain/Repository.js
@@ -0,0 +1,34 @@
+function Repository() {
+  this._data = [];
+
+  Object.defineProperty(this, "getById", {
+    value: function (id) {
+      return this._data.find((item) => item.id === id);
+    },
+    writable: true,
+  });
+}
+
+Repository.prototype.add = function (item) {
+  this._data.push(item);
+};
+
+Repository.prototype.get = function (id) {
+  return this._data.find((item) => item.id === id);
+};
+
+Repository.prototype.getAll = function () {
+  return this._data;
+};
+
+Repository.prototype.update = function (id, item) {
+  const index = this._data.findIndex((item) => item.id === id);
+  this._data.splice(index, 1, item);
+};
+
+Repository.prototype.delete = function (id) {
+  const index = this._data.findIndex((item) => item.id === id);
+  this._data.splice(index, 1);
+};
+
+module.exports = Repository;
