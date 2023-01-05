8c2865a52cb689d2443790df2c1be04b95cb6d31
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-05 21:01:08 +0100

    Add bycicle repository object testing and integration testing

--- tests/data/bycicleRepository.test.js
+++ tests/data/bycicleRepository.test.js
@@ -1,14 +1,39 @@
 const database = require("../../database");
 const Model = require("../../domain/models/bycicle.js");
 const { expect } = require("@jest/globals");
-const bycicleRepository = require("../../Domain/BycicleRepository.js");
+const ObjectId = require("mongodb").ObjectId;
+const BycicleRepository = require("../../Domain/BycicleRepository.js");
+const Repository = require("../../Domain/Repository");
+
+describe("Bycicle repository should override prototype chain inherit methods", () => {
+  test("Repository has own prototype methods", () => {
+    // Arrange
+    const sut = new Repository();
+    expect(sut.hasOwnProperty("getAll")).toBeTruthy();
+    expect(sut.hasOwnProperty("getById")).toBeTruthy();
+  });
+
+  test("BycicleRepository has own prototype methods", () => {
+    // Arrange
+    const sut = new BycicleRepository();
+    expect(sut.hasOwnProperty("getAll")).toBeTruthy();
+    expect(sut.hasOwnProperty("getById")).toBeTruthy();
+  });
+
+  test("BycicleRepository prototype has own prototype methods", () => {
+    // Arrange
+    const sut = new BycicleRepository(Model);
+    expect(sut.__proto__.hasOwnProperty("getAll")).toBeTruthy();
+    expect(sut.__proto__.hasOwnProperty("getById")).toBeTruthy();
+    expect(sut.__proto__.hasOwnProperty("model")).toBeFalsy();
+  });
+});
 
 let connection;
-
-describe("Mongoose testing", () => {
+describe("Bycicle repositorion implementation/integration tests", () => {
   beforeAll(async () => {
     await database.mongoDbConfig.connect();
-    // await Model.collection.drop();
+    await Model.collection.drop();
   });
 
   afterAll(async () => {
@@ -17,23 +42,60 @@ describe("Mongoose testing", () => {
 
   connection = database.mongoDbConfig.getConnection();
 
-  test("Should connect to database", async () => {
-    expect(connection.readyState).toEqual(1); // Status connected. Not connecting, is different.
+  describe("Connection testing", () => {
+    test("Should connect to database", async () => {
+      expect(connection.readyState).toEqual(1); // Status connected. Not connecting, is different.
+    });
+
+    test("Model should use testing collection", async () => {
+      expect(Model.collection.collectionName).toEqual("Testing");
+    });
   });
 
-  test("Model should use testing collection", async () => {
-    expect(Model.collection.collectionName).toEqual("Testing");
-  });
+  describe("Bycicle repository implementation tests", () => {
+    test("Should retrieve doc from collecion", async () => {
+      // Arrange
+      const sut = new BycicleRepository();
+      const dataUnderTest = new Model({
+        _id: ObjectId("63b1db13b2ade9465c9c5d0d"),
+        name: "Roadster Elite",
+        brand: "Argon 18",
+        price: 1299.99,
+        type: "Road",
+        frame: "Carbon",
+        fork: "XC",
+        gears: "Derailleur gears",
+        brakes: "Hydraulic Disc",
+        wheels: "700c",
+        tires: "Road tires",
+        suspension: "Hardtail",
+        weight: 7.5,
+        available: true,
+      });
 
-  describe("Bycicle repository should override prototype chain method and implement with mongoose", () => {
-    test("Should implementeation search by id", async () => {
-      // Act
-      const result = await bycicleRepository.getById(
-        "5f3d3b8aa2bcfd321e1c9d66"
-      );
+      await dataUnderTest.save();
 
-      // Assert
-      expect(result).not.toBeNull();
+      const contextObjectUnderTest = {
+        id: "63b1db13b2ade9465c9c5d0d",
+        name: "Roadster Elite",
+        brand: "Argon 18",
+        price: 1299.99,
+        type: "Road",
+        frame: "Carbon",
+        fork: "XC",
+        gears: "Derailleur gears",
+        brakes: "Hydraulic Disc",
+        wheels: "700c",
+        tires: "Road tires",
+        suspension: "Hardtail",
+        weight: 7.5,
+        available: true,
+      };
+
+      // Act & assert
+      const result = await sut.getById("63b1db13b2ade9465c9c5d0d");
+      console.log(result);
+      // expect(Object.keys(result)).toEqual(Object.keys(contextObjectUnderTest));
     });
   });
 });
