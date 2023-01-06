fd8257908f66929861d351ba0bf65d67d14abe9f
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-06 18:58:42 +0100

    Add repository tests to check getAll and getByBrand

--- tests/data/bycicleRepository.test.js
+++ tests/data/bycicleRepository.test.js
@@ -33,9 +33,11 @@ let connection;
 describe("Bycicle repositorion implementation/integration tests", () => {
   beforeAll(async () => {
     await database.mongoDbConfig.connect();
-    await Model.collection.drop();
   });
 
+  afterEach(async () => {
+    await database.mongoMigrationConfig.dropCollection();
+  });
   afterAll(async () => {
     await database.mongoDbConfig.disconnect();
   });
@@ -92,14 +94,146 @@ describe("Bycicle repositorion implementation/integration tests", () => {
         available: true,
       };
 
-      // Act & assert
+      // Act &
       const result = await sut.getById("63b1db13b2ade9465c9c5d0d");
       const resultModel = await Model.findById("63b1db13b2ade9465c9c5d0d");
 
+      // Assert
       expect(await Model.estimatedDocumentCount()).toBe(1);
       expect(result).not.toBeNull();
       expect(resultModel).not.toBeUndefined();
       expect(result).not.toBeUndefined();
+    });
+
+    test("Should retrieve all docs from collection", async () => {
+      // Arrange
+      const sut = new BycicleRepository(Model);
+      const dataUnderTest = [
+        new Model({
+          _id: ObjectId("63b859b14552d7ff361a5be5"),
+          name: "Roadster Elite",
+          brand: "Argon 18",
+          price: 1299.99,
+          type: "Road",
+          frame: "Carbon",
+          fork: "XC",
+          gears: "Derailleur gears",
+          brakes: "Hydraulic Disc",
+          wheels: "700c",
+          tires: "Road tires",
+          suspension: "Hardtail",
+          weight: 7.5,
+          available: true,
+        }),
+        new Model({
+          _id: ObjectId("63b85961c2ad6d4b9048f7e1"),
+          name: "Roadster Elite",
+          brand: "Argon 18",
+          price: 1299.99,
+          type: "Road",
+          frame: "Carbon",
+          fork: "XC",
+          gears: "Derailleur gears",
+          brakes: "Hydraulic Disc",
+          wheels: "700c",
+          tires: "Road tires",
+          suspension: "Hardtail",
+          weight: 7.5,
+          available: true,
+        }),
+      ];
+
+      await Model.insertMany(dataUnderTest);
+
+      // Act
+      const result = await sut.getAll();
+
+      // Assert
+      expect(result).not.toBeNull();
+      expect(result.length).toBe(2);
+    });
+
+    test("Should retrieve all docs with same brand from collection", async () => {
+      const sut = new BycicleRepository(Model);
+      const dataUnderTest = [
+        new Model({
+          _id: ObjectId("63b85a6b30b8076e6b5c14ff"),
+          name: "Roadster Elite",
+          brand: "Argon 18",
+          price: 1299.99,
+          type: "Road",
+          frame: "Carbon",
+          fork: "XC",
+          gears: "Derailleur gears",
+          brakes: "Hydraulic Disc",
+          wheels: "700c",
+          tires: "Road tires",
+          suspension: "Hardtail",
+          weight: 7.5,
+          available: true,
+        }),
+        new Model({
+          _id: ObjectId("63b85a7546f4e3e30fc7d97d"),
+          name: "Roadster Elite",
+          brand: "All City",
+          price: 1299.99,
+          type: "Road",
+          frame: "Carbon",
+          fork: "XC",
+          gears: "Derailleur gears",
+          brakes: "Hydraulic Disc",
+          wheels: "700c",
+          tires: "Road tires",
+          suspension: "Hardtail",
+          weight: 7.5,
+          available: true,
+        }),
+        new Model({
+          _id: ObjectId("63b85a7abee199663346070c"),
+          name: "Roadster Elite",
+          brand: "Argon 18",
+          price: 1299.99,
+          type: "Road",
+          frame: "Carbon",
+          fork: "XC",
+          gears: "Derailleur gears",
+          brakes: "Hydraulic Disc",
+          wheels: "700c",
+          tires: "Road tires",
+          suspension: "Hardtail",
+          weight: 7.5,
+          available: true,
+        }),
+        new Model({
+          _id: ObjectId("63b85a7f266a0d3a963d15cc"),
+          name: "Roadster Elite",
+          brand: "CUBE",
+          price: 1299.99,
+          type: "Road",
+          frame: "Carbon",
+          fork: "XC",
+          gears: "Derailleur gears",
+          brakes: "Hydraulic Disc",
+          wheels: "700c",
+          tires: "Road tires",
+          suspension: "Hardtail",
+          weight: 7.5,
+          available: true,
+        }),
+      ];
+
+      await Model.insertMany(dataUnderTest);
+
+      // Act
+      const result = await sut.getByBrand("Argon 18");
+
+      // Assert
+      expect(result).not.toBeNull();
+      expect(result.length).toBe(2);
+      expect(result[0].brand).toBe("Argon 18");
+      result.forEach((doc) => {
+        expect(doc.brand).toBe("Argon 18");
+      });
     });
   });
 });
