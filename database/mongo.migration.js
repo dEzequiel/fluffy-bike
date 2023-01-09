8d96f6d5f6ffb8a3fc5a3c9140e98c49cc633b6b
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-09 10:08:51 +0100

    Prepare migration function

--- database/mongo.migration.js
+++ database/mongo.migration.js
@@ -1,6 +1,8 @@
 const Model = require("../domain/models/bycicle.js");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const mockedBycicles = require("./mocked.bycicles.js");
 
 dotenv.config();
 
@@ -48,38 +50,40 @@ const databaseMigrationModule = (function () {
     }
   }
 
-  // async function migrate() {
-  //   client = new MongoClient(process.env.MONGO_URI_DEVELOPMENT);
-  //   try {
-  //     await client.connect();
-  //     db = client.db("BikehubDB");
-  //     collection = db.collection("Testing");
+  async function migrate() {
+    client = new MongoClient(process.env.MONGO_URI_DEVELOPMENT);
+    try {
+      await client.connect();
+      db = client.db("BikehubDB");
+      collection = db.collection("Testing");
 
-  //     let documents = await collection.estimatedDocumentCount();
-  //     if (documents > 0)
-  //       await collection.drop().then((status) => {
-  //         console.log(`Drop testing collection => ${status}`);
-  //       });
+      let documents = await collection.estimatedDocumentCount();
+      if (documents > 0)
+        await collection.drop().then((status) => {
+          console.log(`Drop testing collection => ${status}`);
+        });
 
-  //     // Actual inserting documents.
-  //     await collection.insertMany(mockedBycicles);
-  //     if (documents > 0) {
-  //       console.log("Seeding testing collection complete!");
-  //     }
+      // Actual inserting documents.
+      await collection.insertMany(mockedBycicles);
+      if (documents > 0) {
+        console.log("Seeding testing collection complete!");
+      }
 
-  //     // Force client.close() to wait for async operations to finish.
-  //     await client.close();
-  //   } catch (ex) {
-  //     console.error(ex.message);
-  //   }
-  // }
+      // Force client.close() to wait for async operations to finish.
+      await client.close();
+    } catch (ex) {
+      console.error(ex.message);
+    }
+  }
 
   return {
     connect: connect,
     disconnect: disconnect,
     getConnection: getConnection,
     dropCollection: dropCollection,
+    migrate: migrate,
   };
 })();
 
+databaseMigrationModule.migrate();
 module.exports = databaseMigrationModule;
