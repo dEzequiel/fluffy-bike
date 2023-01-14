1df35fcb58e80ad0819b1a977d059902f60e70a4
eze <91556738+dEzequiel@users.noreply.github.com>
2023-01-14 15:59:48 +0100

    Fix collection to use

---
+++ Domain/models/stock.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const Schema = mongoose.Schema;
+
+const StockSchema = new mongoose.Schema({
+  shop: { type: Schema.Types.ObjectId },
+  bycicles: [
+    {
+      bycicle: { type: Schema.Types.ObjectId, ref: "Bycicle" },
+      quantity: { type: Number },
+    },
+  ],
+});
+
+if (process.env.NODE_ENV === "development") {
+  StockSchema.set("collection", "Stock-Testing");
+}
+
+module.exports = mongoose.model("Stock", StockSchema);
