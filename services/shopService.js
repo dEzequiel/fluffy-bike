457cda017c3c05f4b046243ab106b3885ea117df
eze <91556738+dEzequiel@users.noreply.github.com>
2023-02-06 19:58:14 +0100

    Delete addBycicleAsync

--- services/shopService.js
+++ services/shopService.js
@@ -2,7 +2,6 @@ const ShopRepository = require("../database/data-access/ShopRepository.js");
 
 const shopService = (function () {
   const repository = new ShopRepository();
-  let parser;
 
   const getByIdAsync = async (id) => {
     const entity = repository.getById(id).then((result) => {
@@ -28,32 +27,10 @@ const shopService = (function () {
     return entity;
   };
 
-  const addBycicleAsync = async (shopId, bycicle) => {
-    const entity = repository
-      .addBycicleToShop(shopId, bycicle)
-      .then((result) => {
-        let pojo = result.toObject();
-
-        let { _id: id, name, bycicles } = pojo;
-
-        return { id, name, bycicles };
-      });
-
-    return entity;
-  };
-  // const getStockAsync = async (id) => {
-  //   const shop = await repository.getStock(id).then((result) => {
-  //     return parser(result);
-  //   });
-  // };
-
-  // Private API for request handlers
-  parser;
 
   return {
     getByIdAsync,
     addAsync,
-    addBycicleAsync,
   };
 })();
 
